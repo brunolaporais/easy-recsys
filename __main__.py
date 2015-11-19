b44e375c758897d1a492780b0e37a0535b144f1b
Bruno Laporais Pereira <brunolaporais@yahoo.com.br>
2015-11-18 22:29:48 -0200

    Update __main__.py
    Migrando para python 3.

--- __main__.py
+++ __main__.py
@@ -7,7 +7,7 @@ def main(argv):
 	input_file = ioo.read_file(argv[0])
 	matrix = input_file.read_matrix()
 	mb = memory_based.memory_based(matrix)
-	print mb.predict_rating(11, 608, knn=30)
+	print(mb.predict_rating(11, 608, knn=30))
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
